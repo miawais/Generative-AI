ca692535cbabb4c19c301906a482b63ce775563b
miawais <awaisbinmukhtar@gmail.com>
2024-10-23 02:45:52 +0500

    database dataloading done

---
+++ SpeakDB/dataloading.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Define your database connection parameters
+username = 'root'
+password = '1043'
+host = 'localhost'  # or your database host
+database = 'employee_data'
+
+engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}/{database}')
+
+
+# Load CSV data into a DataFrame
+df = pd.read_csv('D:/Repos/Employee.csv')
+
+# Load DataFrame into the SQL table
+df.to_sql('employeedetails', con=engine, if_exists='replace', index=False)
